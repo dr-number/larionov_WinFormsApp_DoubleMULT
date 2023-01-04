228ef1856de55f3daba3c6ceaf8db6778a0d8704
Lar97 <dr.number@yandex.ru>
2023-01-04 18:42:07 +0700

    Add project files.

---
+++ larionov_WinFormsApp_DoubleMULT/Form1.cs
@@ -0,0 +1,10 @@
+namespace larionov_WinFormsApp_DoubleMULT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
